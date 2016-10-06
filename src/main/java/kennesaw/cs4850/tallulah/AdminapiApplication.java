5bc850c75b2fc139f3483d1fbd2bbd9fb8e93f22
Jun Nguyen <Jun Nguyen>
2016-10-06 16:08:51 -0400

    initial commit

---
+++ src/main/java/kennesaw/cs4850/tallulah/AdminapiApplication.java
@@ -0,0 +1,12 @@
+package kennesaw.cs4850.tallulah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminapiApplication.class, args);
+	}
+}
---
+++ src/test/java/kennesaw/cs4850/tallulah/AdminapiApplicationTests.java
@@ -0,0 +1,16 @@
+package kennesaw.cs4850.tallulah;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
